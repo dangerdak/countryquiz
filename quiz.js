e1750fa4f069288abe71d27ae507f7defb594ac6
dangerdak <dakshina.scott@gmail.com>
2014-05-16 22:09:53 +0100

    Changed order of code

--- quiz.js
+++ quiz.js
@@ -1,60 +1,3 @@
-window.onload = function(){
-"use strict";
-	currentQuestion.display();
-};
-
-
-// Display, change and retrieve info from current question
-var currentQuestion = {
-	number: 0,
-	next: function() {
-		"use strict";
-		this.number += 1;
-	},
-	display: function() {
-		"use strict";
-		injectNumber(this.number +1);
-		injectCountry(this.number);
-		injectChoices(this.number);
-	},
-	get country() {
-		"use strict";
-		return questions[this.number].countryName;
-	}, 
-	get choices() {
-		"use strict";
-		return questions[this.number].choices;
-	}
-};
-
-// For placing question info into HTML document
-//Insert the name of the country which the question is about
-function injectCountry(questionNumber) {
-	"use strict";
-	var element = document.getElementById("country");
-	var country = document.createTextNode(questions[questionNumber].countryName);
-	element.appendChild(country);
-}
-
-//Insert the choices available for a given country
-function injectChoices(questionNumber) {
-	"use strict";
-	var elements = document.getElementsByClassName("choices");
-	for(var i=0, len = questions.length; i<len; i++) {
-		var option = document.createTextNode(questions[questionNumber].choices[i]);
-		elements[i].appendChild(option);
-	}
-}
-
-//Display question number on page (input should be one greater than array index)
-function injectNumber(questionNumber) {
-	"use strict";
-	var element = document.getElementById("questionNumber");
-	var number = document.createTextNode(questionNumber + ". ");
-	element.appendChild(number);
-
-}
-
 // List of objects. Each object represents a country.
 var questions = 
 	[
@@ -85,4 +28,59 @@ var questions =
 }
 
 	];
-				
+	
+// For placing question info into HTML document
+//Insert the name of the country which the question is about
+function injectCountry(questionNumber) {
+	"use strict";
+	var element = document.getElementById("country");
+	var country = document.createTextNode(questions[questionNumber].countryName);
+	element.appendChild(country);
+}
+
+//Insert the choices available for a given country
+function injectChoices(questionNumber) {
+	"use strict";
+	var elements = document.getElementsByClassName("choices");
+	for(var i=0, len = questions.length; i<len; i++) {
+		var option = document.createTextNode(questions[questionNumber].choices[i]);
+		elements[i].appendChild(option);
+	}
+}
+
+//Display question number on page (input should be one greater than array index)
+function injectNumber(questionNumber) {
+	"use strict";
+	var element = document.getElementById("questionNumber");
+	var number = document.createTextNode(questionNumber + ". ");
+	element.appendChild(number);
+
+}
+
+// Display, change and retrieve info from current question
+var currentQuestion = {
+	number: 0,
+	next: function() {
+		"use strict";
+		this.number += 1;
+	},
+	display: function() {
+		"use strict";
+		injectNumber(this.number +1);
+		injectCountry(this.number);
+		injectChoices(this.number);
+	},
+	get country() {
+		"use strict";
+		return questions[this.number].countryName;
+	}, 
+	get choices() {
+		"use strict";
+		return questions[this.number].choices;
+	}
+};
+	
+window.onload = function(){
+"use strict";
+	currentQuestion.display();
+};
